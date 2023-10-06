98cdf9bd0656f5407b9fe68a2b3035a648d7ccd4
naufalfdlnr <139071247+naufalfdlnr@users.noreply.github.com>
2023-10-06 17:34:35 +0700

    Add files via upload

--- asset/style/slideshow.js
+++ asset/style/slideshow.js
@@ -24,3 +24,22 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+
+function love1() {
+  var l1 = document.getElementById("love1");
+  var l2 = document.getElementById("love2");
+  l1.style.visibility = 'hidden'
+  l2.style.visibility = 'visible'
+}
+
+function love2() {
+  var l1 = document.getElementById("love1");
+  var l2 = document.getElementById("love2");
+  l1.style.visibility = 'visible'
+  l2.style.visibility = 'hidden'
+
+}
+
+document.getElementById("defaultOpen").click();
